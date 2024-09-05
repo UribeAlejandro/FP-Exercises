98a89a296eea38950419b1f60eaf273f5f3a94e0
Alejandro Uribe <jauriberamirez@gmail.com>
2024-09-05 20:26:05 -0300

    Added more exercises and their tests

--- src/main/scala/basic/ArraysExercises.scala
+++ src/main/scala/basic/ArraysExercises.scala
@@ -1,6 +1,5 @@
 package basic
 
-
 object ArraysExercises {
   // https://www.hackerrank.com/challenges/fp-list-replication/problem?isFullScreen=true
   def listReplication(n: Int, arr: List[Int]): List[Int] = {
@@ -8,20 +7,32 @@ object ArraysExercises {
   }
 
   // https://www.hackerrank.com/challenges/fp-filter-array/problem?isFullScreen=true
-  def filterByDelimiter(delim:Int, arr:List[Int]):List[Int] = {
+  def filterByDelimiter(delim: Int, arr: List[Int]): List[Int] = {
     arr.filter(_ < delim)
   }
   // https://www.hackerrank.com/challenges/fp-filter-positions-in-a-list/problem?isFullScreen=true
-  def filterPositions(arr:List[Int]):List[Int] = {
+  def filterPositions(arr: List[Int]): List[Int] = {
     arr.zipWithIndex.filter(_._2 % 2 != 0).map(_._1)
   }
 
   // https: //www.hackerrank.com/challenges/fp-array-of-n-elements/problem?isFullScreen=true
-  def arrayOfNElements(n:Int): List[Int] = {
+  def arrayOfNElements(n: Int): List[Int] = {
     (0 until n).toList
   }
   // https://www.hackerrank.com/challenges/fp-reverse-a-list/problem?isFullScreen=false
-  def reverseList(arr:List[Int]):List[Int] = {
+  def reverseList(arr: List[Int]): List[Int] = {
     arr.reverse
+  }
+  // https://www.hackerrank.com/challenges/fp-sum-of-odd-elements/problem?isFullScreen=true
+  def sumOddElements(arr: List[Int]): Int = {
+    arr.filter(_ % 2 != 0).sum
+  }
+  // https://www.hackerrank.com/challenges/fp-list-length/problem?isFullScreen=true
+  def listLength(arr: List[Int]): Int = {
+    arr.length
+  }
+  // https://www.hackerrank.com/challenges/fp-update-list/problem?isFullScreen=true
+  def updateList(arr: List[Int]): List[Int] = {
+    arr.map(_.abs)
   }
 }
--- src/test/scala/basic/ArraysExercisesTest.scala
+++ src/test/scala/basic/ArraysExercisesTest.scala
@@ -5,31 +5,55 @@ import org.scalatest.funsuite.AnyFunSuiteLike
 class ArraysExercisesTest extends AnyFunSuiteLike {
 
   test("testFilterArray") {
-    val arr = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val delim = 5
+    val arr      = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val delim    = 5
     val expected = List(1, 2, 3, 4)
-    val result = ArraysExercises.filterByDelimiter(delim, arr)
+    val result   = ArraysExercises.filterByDelimiter(delim, arr)
     assert(result == expected)
   }
 
   test("testListReplication") {
-    val n = 3
-    val arr = List(1, 2, 3, 4)
+    val n        = 3
+    val arr      = List(1, 2, 3, 4)
     val expected = List(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4)
-    val result = ArraysExercises.listReplication(n, arr)
+    val result   = ArraysExercises.listReplication(n, arr)
     assert(result === expected)
   }
 
   test("testFilterPositions") {
-    val arr = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val arr      = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val expected = List(2, 4, 6, 8, 10)
-    val result = ArraysExercises.filterPositions(arr)
+    val result   = ArraysExercises.filterPositions(arr)
     assert(result === expected)
   }
   test("testArrayOfNElements") {
-    val n = 5
+    val n        = 5
     val expected = List(0, 1, 2, 3, 4)
-    val result = ArraysExercises.arrayOfNElements(n)
+    val result   = ArraysExercises.arrayOfNElements(n)
+    assert(result === expected)
+  }
+  test("testReverseArray") {
+    val arr      = List(1, 2, 3, 4, 5)
+    val expected = List(5, 4, 3, 2, 1)
+    val result   = ArraysExercises.reverseList(arr)
+    assert(result === expected)
+  }
+  test("testSumOddElements") {
+    val arr      = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val expected = 25
+    val result   = ArraysExercises.sumOddElements(arr)
+    assert(result === expected)
+  }
+  test("testListLength") {
+    val arr      = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val expected = 10
+    val result   = ArraysExercises.listLength(arr)
+    assert(result === expected)
+  }
+  test("testUpdateList") {
+    val arr      = List(-1, 2, -3, 4, -5, 6, -7, 8, -9, 10)
+    val expected = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result   = ArraysExercises.updateList(arr)
     assert(result === expected)
   }
 }
